5eca405b63876c8877d960e236a3bbefc7452d58
Inderpreet Singh <er.inderpreet@gmail.com>
2018-09-04 20:15:00 +0530

    Initial Working Version.

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+/**
+*   \description Entry Point File
+*/
+
+#include "mutex1.h"
+
+int main(void){
+
+    printHello();
+
+    return 0;
+}
---
+++ src/mutex1.cpp
@@ -0,0 +1,10 @@
+/**
+*   \Description Program to test Mutexes
+*/
+
+#include <stdio.h>
+#include "mutex1.h"
+
+void printHello(void){
+    printf("Hello World\n\r");
+}
